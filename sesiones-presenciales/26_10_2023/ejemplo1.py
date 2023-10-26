ee96d2279c8c63b0d38723339555b45726cf9f24
tigarto <helpu1409@gmail.com>
2023-10-26 00:00:03 -0500

    agregando ejemplo exponencial (sin resover)

---
+++ sesiones-presenciales/26_10_2023/ejemplo1.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Antes de empezar: %matplotlib qt
+
+Se supone que el Sky Train llega cada ocho minutos desde la terminal hasta el centro de
+alquiler de automóviles y el estacionamiento de larga duración. Se sabe que los tiempos de
+espera del tren siguen una distribución uniforme.
+a. ¿Cuál es el tiempo promedio de espera (en minutos)? (Rta: 4)
+b. Halle el percentil 30 de los tiempos de espera (en minutos).
+c. ¿Cuál es la probabilidad de esperar más de siete minutos dado que una persona ha
+   esperado más de cuatro minutos? (Rta: 0.25)
+   
+   
+En un centro de atención de urgencias los pacientes llegan a una tasa promedio de un
+paciente cada siete minutos. Supongamos que la duración entre llegadas se distribuye
+exponencialmente.
+a. Calcule la probabilidad de que el tiempo entre dos visitas sucesivas al centro de 
+   atención de urgencias sea inferior a 2 minutos. (Rta: 0.2485)
+b. Calcule la probabilidad de que el tiempo entre dos visitas sucesivas al centro de
+   atención de urgencias sea superior a 15 minutos. (Rta: 0.1173)
+c. Si han pasado 10 minutos desde la última llegada, ¿cuál es la probabilidad de que 
+   la siguiente persona llegue durante los próximos cinco minutos? (Rta: 0.4875)
+d. Calcule la probabilidad de que lleguen más de ocho pacientes durante un periodo de
+   media hora. (Rta: 0.0311)   
+"""
+
+import scipy.stats as stats
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.close('all') # Se cierran todas las figuras para empezar
+
+"""
+Definición de la VA - Exponencial
+
+X ~ 
+
+X: 
+
+Parametros:
+"""
+
+
+
+# a. Calcule la probabilidad de que el tiempo entre dos visitas sucesivas al centro 
+#    de atención de urgencias sea inferior a 2 minutos. (Rta: 0.2485)
+
+# b. Calcule la probabilidad de que el tiempo entre dos visitas sucesivas al centro 
+#    de atención de urgencias sea superior a 15 minutos. (Rta: 0.1173)
+
+
+# c. Si han pasado 10 minutos desde la última llegada, ¿cuál es la probabilidad de 
+#    que la siguiente persona llegue durante los próximos cinco minutos? (Rta: 0.4875)
+
+
+# d. Calcule la probabilidad de que lleguen más de ocho pacientes durante un periodo 
+#    de media hora. (Rta: 0.0311)  
+
+
